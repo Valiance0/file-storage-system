61cec9ebcc3558c763650c06f1c6170f0666abce
Valiance <195592303+Valiance0@users.noreply.github.com>
2026-01-23 11:40:52 -0600

    Added Post/ upload file endpoint

--- source/main.py
+++ source/main.py
@@ -1,15 +1,34 @@
-from fastapi import FastAPI, Depends
+import os
+import shutil
+from fastapi import FastAPI, Depends, HTTPException, UploadFile
 from sqlmodel import SQLModel, Session, select
 from database import engine, get_database
-from schema import File, User
+from schema import File as FileDB, User
 
 SQLModel.metadata.create_all(engine)
 
 app = FastAPI()
 
-@app.get("/")
-def get_page():
-    return {"message": "Online"}
+@app.post("/upload")
+def upload_file(file: UploadFile, database:Session = Depends(get_database)):
+    filename = file.filename
+    if not filename:
+        raise HTTPException(status_code=400, detail="Missing file name.") 
+    
+    file_path = f"../storage/{filename}"
+    with open(file_path, 'wb') as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    
+    file_size = os.path.getsize(file_path)
+
+    new_file = FileDB(filename = filename, file_path = file_path, file_hash = "temp", size_in_bytes=file_size)
+
+    database.add(new_file)
+    database.commit()
+    database.refresh(new_file)
+
+    return {"status":"success", "file_id": new_file.id, "filename" : new_file.filename}
+
 
 @app.get("/show_database")
 def show_database(database: Session = Depends(get_database)):
