f21d1a62f6652f18418c0e2ad3f556dfea775940
Valiance <195592303+Valiance0@users.noreply.github.com>
2026-01-30 00:23:27 -0600

    Removed stray slash.

--- source/schema.py
+++ source/schema.py
@@ -19,7 +19,7 @@ class UserSession(SQLModel, table=True):
 class UserFile(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     filename: str = Field(index=True)
-    upload_date: datetime = Field(default_factory=get_current_time)\
+    upload_date: datetime = Field(default_factory=get_current_time)
 
     user_id: int = Field(foreign_key = "user.id")
     blob_id: int = Field(foreign_key = "fileblob.id")
